3ec7db002d9825d033d2d9e17d6db78dc914b760
Precious Adejumola <olubunmiprecious195@gmail.com>
2024-07-25 16:38:06 +0100

    add all my codes for triangleTracker

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    // Function to check if triangle is valid
+    $("#checkTriangle").click(function () {
+        let side1 = parseInt($("#side1").val());
+        let side2 = parseInt($("#side2").val());
+        let side3 = parseInt($("#side3").val());
+        $("#triangle_cont").hide();
+        $("#result").show();
+        $("input").val("");
+
+        if (side1 === side2 && side2 === side3) {
+            $("#result").text("its an Equilateral triangle");
+        } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+            $("#result").text("its an isosceles triangle");
+        }else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
+            $("#result").text("its a scalene triangle");
+        } else {
+            ($("#result").text("Not a valid triangle"))
+        }
+    })
+    $("#result").click(function(){
+        $("#result").hide();
+        $("#triangle_cont").show();
+    });
+});

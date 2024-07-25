3eb1640a557d8653a2e7f82f78860e6e789e9eb3
Precious Adejumola <olubunmiprecious195@gmail.com>
2024-07-25 17:27:27 +0100

    add correct code to triangleTracker

--- js/script.js
+++ js/script.js
@@ -8,17 +8,24 @@ $(document).ready(function () {
         $("#result").show();
         $("input").val("");
 
-        if (side1 === side2 && side2 === side3) {
-            $("#result").text("its an Equilateral triangle");
-        } else if (side1 === side2 || side1 === side3 || side2 === side3) {
-            $("#result").text("its an isosceles triangle");
-        }else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
-            $("#result").text("its a scalene triangle");
+        if (side1 > 0 && side2 > 0 && side3 > 0) {
+            if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
+                if (side1 === side2 && side2 === side3) {
+                    $("#result").text("its an Equilateral triangle");
+                } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+                    $("#result").text("its an isosceles triangle");
+                } else {
+                    $("#result").text("its a scalene triangle");
+                }
+            } else {
+                $("#result").text("Not a valid triangle");
+            }
         } else {
-            ($("#result").text("Not a valid triangle"))
+            $("#result").text("Not a valid triangle");
         }
+        
     })
-    $("#result").click(function(){
+    $("#result").click(function () {
         $("#result").hide();
         $("#triangle_cont").show();
     });

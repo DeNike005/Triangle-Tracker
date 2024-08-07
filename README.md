3244dd713fcc12ccf4de9143582c933f10ef2bbb
Precious Adejumola <olubunmiprecious195@gmail.com>
2024-08-07 22:16:55 +0100

    add changes to my code

---
+++ README.md
@@ -0,0 +1,42 @@
+# _ Triangle Tracker_
+
+#### _This project is a simple web-based application for determining the type of a triangle based on the lengths of its sides._
+
+#### By _**PRECIOUS DENIKE**_
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _GIT_
+* _javaScript_
+* _jquery_
+
+
+## Description
+
+_Features
+
+-Input Validation: Ensures all side lengths are positive numbers.
+
+-Triangle Type Identification: Determines if the input sides form an equilateral, isosceles, or scalene triangle.
+
+-User Interface: Provides an easy-to-use interface for inputting side lengths and displaying results._
+
+## Setup/Installation Requirements
+
+* _Clone this repository to your system_
+* _Navigate to top directory_
+* _Open Index.html_
+
+## Known Bugs
+
+* _No links working_
+
+## License
+
+_MIT_
+
+## Contact Information
+
+_Email:Olubunmiprecious195@gmail.com_
--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
 $(document).ready(function () {
-    // Function to check if triangle is valid
     $("#checkTriangle").click(function () {
         let side1 = parseInt($("#side1").val());
         let side2 = parseInt($("#side2").val());
         let side3 = parseInt($("#side3").val());
         $("#triangle_cont").hide();
         $("#result").show();
-        $("input").val("");
+
+        let side1AddSide2 = side1 + side2;
+        let side1AddSide3 = side1 + side3;
+        let side2AddSide3 = side2 + side3;
+        // $("input").val("");
 
         if (side1 > 0 && side2 > 0 && side3 > 0) {
-            if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
-                if (side1 === side2 && side2 === side3) {
-                    $("#result").text("its an Equilateral triangle");
-                } else if (side1 === side2 || side1 === side3 || side2 === side3) {
-                    $("#result").text("its an isosceles triangle");
-                } else {
-                    $("#result").text("its a scalene triangle");
-                }
-            } else {
+             if (side1AddSide2 <= side1 || side1AddSide3 <= side2 || side2AddSide3 <= side3) {
                 $("#result").text("Not a valid triangle");
+            }else if (side1 === side2 && side2 === side3) {
+                $("#result").text("its an Equilateral triangle");
             }
+            else if (side1 === side2 || side1 === side3 || side2 === side3) {
+                $("#result").text("its an isosceles triangle");
+            } else {
+                $("#result").text("its a scalene triangle");
+            } 
         } else {
             $("#result").text("Not a valid triangle");
         }
-        
+
+
     })
     $("#result").click(function () {
         $("#result").hide();
